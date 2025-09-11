6898cd6fa004c36121404bfc3263f425035bdc0b
Lucy Milenov <lucymilenov@Lucys-MacBook-Air.local>
2025-09-12 01:53:23 +0300

    Add validation attributes and custom FutureDate validation for TaskCreateDto

--- TaskManager.API/DTOs/CategoryCreateDto.cs
+++ TaskManager.API/DTOs/CategoryCreateDto.cs
@@ -1,6 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TaskManager.API.DTOs;
 
 public class CategoryCreateDto
 {
+    [Required(ErrorMessage = "Name is required")]
+    [StringLength(50, ErrorMessage = "Name cannot be longer than 50 characters")]
     public string Name { get; set; } = "";
 }
--- TaskManager.API/DTOs/TaskCreateDto.cs
+++ TaskManager.API/DTOs/TaskCreateDto.cs
@@ -1,11 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using TaskManager.API.Validation;
+
 namespace TaskManager.API.DTOs;
 
 public class TaskCreateDto
 {
+    [Required(ErrorMessage = "Title is required")]
+    [StringLength(100, ErrorMessage = "Tittle cannot be longer that 100 characters")]
     public string Title { get; set; } = "";
+
+    [Required(ErrorMessage = "Description is required")]
+    [StringLength(500, ErrorMessage = "Description cannot be longer than 500 characters")]
     public string Description { get; set; } = "";
+
+    [Required]
+    [DataType(DataType.Date)]
+    [FutureDate(ErrorMessage = "Due date must be in the future")]
     public DateTime DueDate { get; set; }
     public bool IsCompleted { get; set; }
 
+    [Required(ErrorMessage = "CategoryId is required")]
     public int CategoryId { get; set; }
 }
