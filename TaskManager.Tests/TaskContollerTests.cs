e4fd6d5fe60a74c44c08518023b2d4b1527b4c79
Lucy Milenov <lucymilenov@Lucys-MacBook-Air.local>
2025-09-13 00:39:43 +0300

    Add first unit test project with in-memory EF Core and basic task test

---
+++ TaskManager.Tests/TaskContollerTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using TaskManager.API.Controllers;
+using TaskManager.API.Data;
+using TaskManager.API.DTOs;
+using TaskManager.API.Models;
+using Xunit;
+
+namespace TaskManager.Tests;
+
+public class TaskContollerTests
+{
+    private AppDbContext GetInMemoryDbContext()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>().UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+        return new AppDbContext(options);
+    }
+}
