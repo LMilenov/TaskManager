a40db8d4bba5f2277b087d0608fa23340f1dbba5
Lucy Milenov <lucymilenov@Lucys-MacBook-Air.local>
2025-09-13 09:35:45 +0300

    Add unit tests for CategoriesController (Add & Delete)

---
+++ TaskManager.Tests/CategoryController.Tests.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskManager.API.Controllers;
+using TaskManager.API.Data;
+using TaskManager.API.DTOs;
+using TaskManager.API.Models;
+using Xunit;
+
+namespace TaskManager.Tests;
+
+public class CategoriesControllerTests
+{
+    private AppDbContext GetInMemoryDbContext()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>().UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+
+        return new AppDbContext(options);
+    }
+
+    [Fact]
+    public void AddCategory_ShouldSaveCategoryInDatabase()
+    {
+        var context = GetInMemoryDbContext();
+        var controller = new CategoriesController(context);
+
+        var dto = new CategoryCreateDto { Name = "Work" };
+
+        controller.AddCategory(dto);
+        var categories = context.Categories.ToList();
+
+        Assert.Single(categories);
+        Assert.Equal("Work", categories[0].Name);
+    }
+
+    [Fact]
+    public void DeleteCategory_ShouldRemoveCategory_WhenExists()
+    {
+        var context = GetInMemoryDbContext();
+        var controller = new CategoriesController(context);
+
+        var category = new Category { Name = "Temp" };
+        context.Categories.Add(category);
+        context.SaveChanges();
+
+        controller.DeleteCategory(category.Id);
+
+        Assert.Empty(context.Categories.ToList());
+    }
+}
